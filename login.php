04cd213ccf5149b5227bb0050e0fd30a033d11f1
perlato2u <perlato2u@etu.univ-lorraine.fr>
2024-01-15 20:22:37 +0100

    connect BDD login

--- login.php
+++ login.php
@@ -11,14 +11,51 @@
             require('header.php');
         ?>
         <div class="container glass" id="signup-form">
-            <form action= method="post">
+            <form method="post">
                 <label for="email">Adresse Mail</label>
                 <input name="email" id="email" class="input-form glass" type="text">
+
                 <label for="password">Mot de passe</label>
                 <input name="password" id="password" class="input-form glass" type="password">
+
                 <input type="submit" class="submit-form glass">
                 <a href=register.php>Register</a>
             </form>
         </div>
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $inputEmail = $_POST["email"];
+                $inputPassword = $_POST["password"];
+                try {
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "sddv_plongee";
+
+                    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                    $sql = "SELECT * FROM utilisateurs WHERE mail = ? AND mdp = ?"; 
+                    $stmt = $pdo->prepare($sql);
+
+                    $stmt->execute([$inputEmail, $inputPassword]);
+                    $user = $stmt->fetch();
+                    session_start();
+                    if (isset($user["id_user"])) {
+                        echo 'Connexion reussie';
+                        $_SESSION["connected"] = true;
+                        header("Location: index.php");
+                    } 
+                    else {
+                        echo $inputEmail."   &   ".$inputPassword;
+                        echo "<a class='text'><br>Identifiant ou mot de passe incorrect.</a>";
+                    }
+                } 
+                catch (PDOException $e) {
+                    echo "Erreur de connexion à la base de données : " . $e->getMessage();
+                }
+            }
+        ?>
     </body>
 </html>
