087082d8d5161ee82dc761fb7c950867b9c2f11c
Leon LeBerre <leon.le-berre5@etu.univ-lorraine.fr>
2024-01-16 10:26:46 +0100

    ajout parametre port

--- dbconnect.php
+++ dbconnect.php
@@ -3,10 +3,11 @@ define('USER',"root");
 define('PASSWD',""); 
 define('SERVER',"localhost"); 
 define('BASE',"sddv_plongee"); 
+define('PORT',"3306"); 
 
 
 function dbconnect(){ 
-  $dsn="mysql:dbname=".BASE.";host=".SERVER; 
+  $dsn="mysql:dbname=".BASE.";host=".SERVER.";port=".PORT; 
   try{ 
     $connexion=new PDO($dsn,USER,PASSWD); 
     $connexion->exec("set names utf8"); //Support utf8
